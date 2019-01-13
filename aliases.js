56617d13c0f3bdcd84b09ab51dae94874b5c70ee
Travis CI User <travis@example.org>
2019-01-13 23:18:30 +0000

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,7 @@
+var ALIASES = {};
+ALIASES['failure_derive'] = {};
+ALIASES['proc_macro2'] = {};
+ALIASES['quote'] = {};
+ALIASES['syn'] = {};
+ALIASES['synstructure'] = {};
+ALIASES['unicode_xid'] = {};
---
+++ i686-unknown-freebsd/implementors/core/str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nix"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"nix/sys/signal/enum.Signal.html\" title=\"enum nix::sys::signal::Signal\">Signal</a>",synthetic:false,types:["nix::sys::signal::Signal"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
